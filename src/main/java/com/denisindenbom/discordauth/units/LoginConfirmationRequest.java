1d0cf4268306e01869940c55ade4a06a03161f05
Sukidesu-Arata <zuizeyt@gmail.com>
2025-05-19 22:34:34 +0700

    Update LoginConfirmationRequest.java

--- src/main/java/com/denisindenbom/discordauth/units/LoginConfirmationRequest.java
+++ src/main/java/com/denisindenbom/discordauth/units/LoginConfirmationRequest.java
@@ -1,37 +1,19 @@
-@Override
-public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-    // Tắt logic xử lý emoji
-    /*
-    String messageId = event.getMessageId();
-    if (event.getUser() == null) return;
-    if (event.getChannelType() != ChannelType.PRIVATE || event.getUser().isBot()) return;
+package com.denisindenbom.discordauth.units;
 
-    if (Emoji.fromUnicode("U+2705").equals(event.getEmoji())) {
-        LoginConfirmationRequest loginConfirmationRequest =
-                this.plugin.getLoginConfirmationRequestManager().getLoginConfirmationRequest(messageId);
+public class LoginConfirmationRequest
+{
+    private final String id;
+    private final Account account;
 
-        if (loginConfirmationRequest == null) return;
-
-        String id = loginConfirmationRequest.getId();
-        Account account = loginConfirmationRequest.getAccount();
-        Player player = this.plugin.getServer().getPlayer(account.getName());
-
-        this.plugin.getLoginConfirmationRequestManager().removeRequest(id);
-
-        if (player == null) {
-            this.plugin.getBot().sendError(this.messagesConfig.getString("bot_error.login"), event.getChannel());
-            return;
-        }
-
-        this.plugin.getAuthManager().addAccount(account);
-
-        this.plugin.getBot().sendSuccessful(this.messagesConfig.getString("bot.login"), event.getChannel());
-
-        event.getChannel().deleteMessageById(messageId).queueAfter(15, TimeUnit.SECONDS);
-
-        this.plugin.getLogger().info(player.getName() + " logged in!");
-
-        this.messageSender.sendMessage(player, this.messagesConfig.getString("welcome"), "{%username%}", player.getName());
+    public LoginConfirmationRequest(String id, Account account)
+    {
+        this.id = id;
+        this.account = account;
     }
-    */
+
+    public String getId()
+    {return this.id;}
+
+    public Account getAccount()
+    {return this.account;}
 }
