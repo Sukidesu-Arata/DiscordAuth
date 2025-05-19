d0e7414f29a027da39fe3b641322c546724c4857
Sukidesu-Arata <zuizeyt@gmail.com>
2025-05-19 22:33:12 +0700

    Update AccountAuthManager.java

--- src/main/java/com/denisindenbom/discordauth/managers/AccountAuthManager.java
+++ src/main/java/com/denisindenbom/discordauth/managers/AccountAuthManager.java
@@ -1,3 +1,39 @@
-public boolean accountExists(String name) {
-    return true; // Coi tất cả người chơi là đã xác thực
+package com.denisindenbom.discordauth.managers;
+
+import com.denisindenbom.discordauth.units.Account;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountAuthManager
+{
+    private final List<Account> authenticatedAccounts = new ArrayList<>();
+
+    public AccountAuthManager()
+    {}
+
+    public void addAccount(Account account)
+    {
+        synchronized (this.authenticatedAccounts)
+        {this.authenticatedAccounts.add(account);}
+    }
+
+    public void removeAccountByName(String name)
+    {
+        synchronized (this.authenticatedAccounts)
+        {
+            for (Account account : this.authenticatedAccounts)
+            {
+                if (account.getName().equals(name))
+                {
+                    this.authenticatedAccounts.remove(account);
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean accountExists(String name)
+    {
+        return true; // Coi tất cả người chơi là đã xác thực
+    }
 }
