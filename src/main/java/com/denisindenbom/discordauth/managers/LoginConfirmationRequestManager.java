b37c802d9523c92a44c2b9e21b352e46d2f0e56c
Sukidesu-Arata <zuizeyt@gmail.com>
2025-05-19 22:38:26 +0700

    Update LoginConfirmationRequestManager.java

--- src/main/java/com/denisindenbom/discordauth/managers/LoginConfirmationRequestManager.java
+++ src/main/java/com/denisindenbom/discordauth/managers/LoginConfirmationRequestManager.java
@@ -1,19 +1,75 @@
-package com.denisindenbom.discordauth.units;
+package com.denisindenbom.discordauth.managers;
 
-public class LoginConfirmationRequest
-{
-    private final String id;
-    private final Account account;
+import com.denisindenbom.discordauth.managers.LoginConfirmationRequestManager.1;
+import com.denisindenbom.discordauth.units.Account;
+import com.denisindenbom.discordauth.units.LoginConfirmationRequest;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Timer;
 
-    public LoginConfirmationRequest(String id, Account account)
-    {
-        this.id = id;
-        this.account = account;
-    }
+public class LoginConfirmationRequestManager {
+   private final List<LoginConfirmationRequest> requests = new ArrayList();
+   private final long lifeTimeOfRequest;
 
-    public String getId()
-    {return this.id;}
+   public LoginConfirmationRequestManager(long lifeTimeOfRequest) {
+      this.lifeTimeOfRequest = lifeTimeOfRequest;
+   }
 
-    public Account getAccount()
-    {return this.account;}
+   public void registerRequest(LoginConfirmationRequest confirmation) {
+      synchronized(this.requests) {
+         this.requests.add(confirmation);
+      }
+
+      (new Timer()).schedule(new 1(this, confirmation), this.lifeTimeOfRequest * 1000L);
+   }
+
+   public void removeRequest(String id) {
+      synchronized(this.requests) {
+         Iterator var3 = this.requests.iterator();
+
+         while(var3.hasNext()) {
+            LoginConfirmationRequest loginConfirmationRequest = (LoginConfirmationRequest)var3.next();
+            if (loginConfirmationRequest.getId().equals(id)) {
+               this.requests.remove(loginConfirmationRequest);
+               break;
+            }
+         }
+
+      }
+   }
+
+   public boolean accountHasRequest(Account account) {
+      synchronized(this.requests) {
+         Iterator var3 = this.requests.iterator();
+
+         LoginConfirmationRequest request;
+         do {
+            if (!var3.hasNext()) {
+               return false;
+            }
+
+            request = (LoginConfirmationRequest)var3.next();
+         } while(!request.getAccount().getName().equals(account.getName()));
+
+         return true;
+      }
+   }
+
+   public LoginConfirmationRequest getLoginConfirmationRequest(String id) {
+      synchronized(this.requests) {
+         Iterator var3 = this.requests.iterator();
+
+         LoginConfirmationRequest loginConfirmationRequest;
+         do {
+            if (!var3.hasNext()) {
+               return null;
+            }
+
+            loginConfirmationRequest = (LoginConfirmationRequest)var3.next();
+         } while(!loginConfirmationRequest.getId().equals(id));
+
+         return loginConfirmationRequest;
+      }
+   }
 }
